5a0713881a054880edf6adc78c023aaad630db84
admin <sappysf123@gmail.com>
2024-01-27 20:45:32 +0300

    END TASK + BOOTSTRAP

---
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
@@ -0,0 +1,29 @@
+package ru.kata.spring.boot_security.demo.entity;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String roleName;
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+    @Override
+    public String getAuthority() {
+        return roleName;
+    }
+}
